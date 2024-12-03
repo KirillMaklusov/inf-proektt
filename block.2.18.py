b606ea247ee490d9e289c1f79aa10238580ce51a
KirillMaklusov <levcenkokirill317@gmail.com>
2024-12-03 08:10:33 +0000

    a

---
+++ block.2.18.py
@@ -0,0 +1,38 @@
+# a)
+
+s = 5
+print(s)
+
+s = 57
+print(s)
+# б)
+s = 6 
+print(s)
+
+
+s = -5.2 * s
+print(s)
+
+
+s = 0
+print(s)
+
+# в)
+s = -7.5
+print(s)
+try:
+    s = 2 * s
+    print(s)
+except NameError as e:
+    print(e)
+
+# г)
+
+s = 45
+print(s)
+
+s = -25
+print(s)
+
+s = s + k
+print(s)

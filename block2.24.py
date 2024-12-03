411d068cdcacfa795915b3d2a0fecb4bbc6a4555
KirillMaklusov <levcenkokirill317@gmail.com>
2024-12-03 10:39:48 +0000

    a

--- block2.24.py
+++ block2.24.py
@@ -1,5 +1,6 @@
 from math import sqrt, sin
 a = int(input("Введите число: "))
-
-print(f"sqrt(2 * a) + sin(abs(3 * a)) / 3,56 = {sqrt(2 * a) + \
-                                                 sin(abs(3 * a)) / 3.56}")
+x = int(input("Введите число x: "))
+# print(f"sqrt(2 * a) + sin(abs(3 * a)) / 3,56 = {sqrt(2 * a) + \
+#                                                  sin(abs(3 * a)) / 3.56}")
+print(f"sin(3.2 + sqrt(1 - x) / abs(5 * x)) = {sin(3.2 + sqrt(1 + x) / abs(5 * x)) }")
